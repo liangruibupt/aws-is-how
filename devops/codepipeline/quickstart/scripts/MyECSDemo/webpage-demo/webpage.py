d5f479602ac62607bab9e9270e08d15756b7b790
liangrui <liangrui.bupt@gmail.com>
2020-08-17 03:34:17 +0000

    MyECSDemo

---
+++ devops/codepipeline/quickstart/scripts/MyECSDemo/webpage-demo/webpage.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def webpage():
+    return render_template('webpage.html',title='webpage')
+
+if __name__ == '__main__':
+    app.run(threaded=True,host='0.0.0.0',port=8080)
